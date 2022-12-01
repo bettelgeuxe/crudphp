4835425b3a157841068d9500fc08abee1e4e82e2
Catherine Escobar Trujillo <bettelgeuxe@hotmail.com>
2022-12-01 17:29:42 -0500

    listadophp

--- CRUD-con-PHP-MYSQL-y-BOOTSTRAP_5-master/index.php
+++ CRUD-con-PHP-MYSQL-y-BOOTSTRAP_5-master/index.php
@@ -16,7 +16,7 @@
   <div class="row justify-content-md-center">
     <div class="col-md-12">
       <h1 class="text-center mt-3">CRUD con PHP - MYSQL y BOOTSTRAP 5 </h1>
-      <p class="text-center">DESDE CERO, <span> AQUÍ SI APRENDERAS ..!</span> </p>
+      
         <a href="./"><i class="bi bi-house"></i></a>
         <hr class="mb-3">
     </div>
