26d22e445fb92b6e11612afbf7e27e0a5b6acbde
Catherine Escobar Trujillo <bettelgeuxe@hotmail.com>
2022-12-03 17:10:37 -0500

    Función buscar

--- CRUD-con-PHP-MYSQL-y-BOOTSTRAP_5-master/buscar.php
+++ CRUD-con-PHP-MYSQL-y-BOOTSTRAP_5-master/buscar.php
@@ -44,65 +44,28 @@
 	 <!-- Imagen de Formulario-->	
 	<div class="container formulario_1">
 		<div id="header">
-            <ul class="nav1">
-                <li><a href="listado.php">Listado</a></li>
-                <li><a class="active" href="buscar.php">Buscar</a></li>
-                <li><a  href="registrar.php">Registrar</a></li>
-                <li><a href="form_consultar.html">Consultar</a></li>
-                <li><a href="form_eliminar.html">Eliminar</a></li>
-            </ul>
-		
-
-        <form class="formulario_1" method="POST" action="buscar.php" enctype="multipart/form-data"><!--enctype para poder enviar archivos como las fotos-->    
-            <p class="elementos_formulario">&nbsp;</p>
-                    <h3 class="elementos_formulario">Colegio San Ignacio</h3>
-                <p class="elementos_formulario">
-			        <label>Nombre y Apellido</label> 
-			        <input type="text" name="namefull">
-			        <label>Nº Documento</label>
-					<input type="number" name="cedula" placeholder="Número de documento">
-			    </p>				
-		        <p class="elementos_formulario">
-		            <label>Género</label>
-		            <input type="radio" name="sexo" value="M" checked>
-                    <label for="sexoM">Masculino</label>
-                    <input type="radio" name="sexo" value="F">
-                    <label for="sexoF">Femenino</label>
-                    <label>Sección</label>
-                    <select name="section" id="section">
-                        <option value="">Asigne una sección</option>
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>
-                    </select> 
-			    </p>
-	            <p class="elementos_formulario">
-			        <label for="formFile">Foto del Usuario</label>
-			        <input name="foto" type="file" accept="image/png,image/jpeg">
-			        
-                </p>
-                <p class="elementos_formulario">
-			        <button type="submit" class="btn-register">
-                        Registrar Usuario
-                        
-                    </button>
-                    <button type="submit" class="btn-register" value="Consultar" name="btn2">
-                        Buscar Usuario
-                        
-                    </button>
-			    </p>
-      </form>
-        </div>
-	</div>
-    
-    <?php 
+        <ul class="nav1">
+          <li><a href="listado.php">Listado</a></li>
+          <li><a class="active" href="buscar.php">Buscar</a></li>
+          <li><a  href="registrar.php">Registrar</a></li>
+          <li><a href="form_consultar.html">Consultar</a></li>
+          <li><a href="form_eliminar.html">Eliminar</a></li>
+        </ul>
+		    <form class="formulario_1" method="POST" action="buscar.php" enctype="multipart/form-data"><!--enctype para poder enviar archivos como las fotos-->    
+          <p class="elementos_formulario">&nbsp;</p>
+            <h3 class="elementos_formulario">Buscar Usuario</h3>
+              <p class="elementos_formulario">
+                
+			          <label>Nº Documento</label>
+					      <input type="number" name="cedula" placeholder="Número de documento">
+			        </p>				
+              <?php 
     if(isset($_POST['btn2']))
     {
       include('config.php');
-      $nombre = $_POST['namefull'];
+      
       $documento = $_POST['cedula'];
-      $genero = $_POST['sexo'];
-      $seccion = $_POST['section'];
+      
       $resultados = mysqli_query($con,"SELECT * FROM table_alumnos WHERE cedula = $documento");
       while($consulta = mysqli_fetch_array($resultados))
       {
@@ -113,7 +76,16 @@
 
       
     ?>
-
+    
+            <button type="submit" class="btn-register" value="Consultar" name="btn2">
+              Buscar Usuario
+            </button>
+			    
+      </form>
+    </div>
+	</div>
+    
+    
     
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="  crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
